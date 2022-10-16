23478c2a593ede34d96d9d0126e26e7a1d86cc90
Nathanael Ostheller <nathanael.ostheller@wsu.edu>
2022-10-16 16:27:15 -0700

    fixed to print names

--- main.py
+++ main.py
@@ -8,28 +8,33 @@ tags = pd.read_csv('./movie-lens-data/tags.csv')
 #create profile of movies and various recieved ratings  
 mainDataFrame = pd.merge(ratings, movies, on="movieId")
 movieMatrix = mainDataFrame.pivot_table(index='userId', columns='title', values='rating')
+movieSeacher = mainDataFrame.pivot_table(index='title', values='movieId')
 
+#computes similarity for move-movie pairs using centered cosine similarity method
 corrMatrix = movieMatrix.corr(method='pearson', min_periods= 50)
 
+#iterates users
 for i in range(1,2): #len(movieMatrix) for final
+    #gets user's ratings
     userRatings = movieMatrix.iloc[i].dropna()
-    print("Ratings for user " + str(i))
-    print(userRatings)
     reccomend = pd.Series()
+
+    #iterates movies
     for j in range(0, len(userRatings)):
-        print("***** Adding similar movies to " + userRatings.index[j] + "*****")
+        #adds similar movies to reccomend table
         similar = corrMatrix[userRatings.index[j]].dropna()
         similar = similar.map(lambda x: x * userRatings[j])
         reccomend = reccomend.append(similar)
-    print("Sorting Reccommendations")
+    
+    #sorts movies in order of best reccomendation
     reccomend.sort_values(inplace= True, ascending= False)
-    print("User Id " + str(i))
-    print(reccomend.head(10))
+
+    #prints top 5 reccomended to file
+    
     x = pd.DataFrame(reccomend)
     reccomendFilter = x[~x.index.isin(userRatings.index)]
-    reccomendFilter.head(5)
-
-    #compute similarity score with centered cosine similarity method
-    #compute 5 item neighborhood set for each movie
-    #estimate rating for users who have not seen
-    #compute top 5 reccommended for each user
+    holdItems = reccomendFilter.head(5)
+    print("User Id " + str(i))
+    for j in range (0, 5):
+        print(holdItems.iloc[j].name)
+    #print(reccomendFilter.head(5))
