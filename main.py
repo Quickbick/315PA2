2ee5f8ed7671189f6cdb26a16dd865fd76391ffe
Nathanael Ostheller <nathanael.ostheller@wsu.edu>
2022-10-16 14:16:00 -0700

    started cleaning and commenting code

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ links = pd.read_csv('./movie-lens-data/links.csv')
 ratings = pd.read_csv('./movie-lens-data/ratings.csv')
 tags = pd.read_csv('./movie-lens-data/tags.csv')
 
+#create profile of movies and various recieved ratings  
 mainDataFrame = pd.merge(ratings, movies, on="movieId")
 movieMatrix = mainDataFrame.pivot_table(index='userId', columns='title', values='rating')
+
 corrMatrix = movieMatrix.corr(method='pearson', min_periods= 50)
 
-for i in range(1,2):
+for i in range(1,2): #len(movieMatrix) for final
     userRatings = movieMatrix.iloc[i].dropna()
     print("Ratings for user " + str(i))
     print(userRatings)
@@ -26,3 +28,8 @@ for i in range(1,2):
     x = pd.DataFrame(reccomend)
     reccomendFilter = x[~x.index.isin(userRatings.index)]
     reccomendFilter.head(5)
+
+    #compute similarity score with centered cosine similarity method
+    #compute 5 item neighborhood set for each movie
+    #estimate rating for users who have not seen
+    #compute top 5 reccommended for each user
